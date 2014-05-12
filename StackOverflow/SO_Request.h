5c234aed90af9d40d2f9d90c216614203f65c850
Loris <loris@reexon.net>
2014-05-12 21:08:15 +0200

    Pulizia SO_Request *Rimossi attributi inutili * Sistemazioni sintattiche (visive)

--- StackOverflow/SO_Request.h
+++ StackOverflow/SO_Request.h
@@ -13,27 +13,12 @@
 //url a cui verrà fatta la richiesta
 @property (nonatomic,weak) NSMutableString *endpoint;
 
-//
-@property (nonatomic,weak) NSNumber *page;
-
-//numero risultati da restituire
-@property (nonatomic,weak) NSNumber *page_size;
-
-@property NSTimeInterval *fromDate;
-
-@property NSTimeInterval *toDate;
-
-//tipo di ordine (desc,asc)
-@property (nonatomic,weak) NSString *order;
-
-//activity,votes,creation
-@property (nonatomic,weak) NSString *sort;
-
-@property (nonatomic,weak) NSString *site;
-
-- (id)initWithType:(int)type parameters:(NSDictionary *)dict;
-- (id)initWithParameters: (int)type questionID:(NSArray *)ids parameters:(NSDictionary *)dict;
+- (id) initWithType:(int)type parameters:(NSDictionary *)dict;
+- (id) initWithParameters: (int)type questionID:(NSArray *)ids parameters:(NSDictionary *)dict;
 
 - (NSArray *)startRequest;
-
+- (void) addQuestionToFavorite:(NSString *)questionID;
+- (void) removeQuestionFromFavorite:(NSString *)questionID;
+- (void) upVoteAnswer:(int)answerID;
+- (void) downVoteAnswer:(int)answerID;
 @end
